70ec38df66469bd84f256d98d978f609a2de2f98
tae <touktw@gmail.com>
2022-12-25 15:03:49 +0900

    [FIX] GfText 정렬, Modifier 이슈 수정

--- component-compose/src/main/java/com/greenlabsfin/design/component/GfText.kt
+++ component-compose/src/main/java/com/greenlabsfin/design/component/GfText.kt
@@ -3,9 +3,12 @@ package com.greenlabsfin.design.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Badge
 import androidx.compose.material.BadgedBox
 import androidx.compose.material.Icon
@@ -36,8 +39,11 @@ import com.greenlabsfin.design.core.color.red60
 fun GfText(
     modifier: Modifier = Modifier,
     text: String,
-    leftIcon: ImageVector? = null,
-    rightIcon: ImageVector? = null,
+    leadingIcon: ImageVector? = null,
+    leadingIconColor: Color = Color.Unspecified,
+    trailingIcon: ImageVector? = null,
+    trailingIconColor: Color = Color.Unspecified,
+    iconAlignment: Alignment.Vertical = Alignment.CenterVertically,
     count: Int? = null,
     countColors: GfCountColors? = null,
     badge: Boolean = false,
@@ -50,66 +56,94 @@ fun GfText(
     enabled: Boolean = true,
 ) {
     // text color priority color -> style -> local provider
-    val textColor = color.takeOrElse {
+    val takenTextColor = color.takeOrElse {
         style.color.takeOrElse {
             LocalGfColorScheme.current.contents.neutralPrimary
         }
     }
 
-    val rememberTextColor by
-    rememberUpdatedState(if (enabled) textColor else textColor.copy(alpha = .3f))
+    val takenLeadingIconColor = leadingIconColor.takeOrElse { takenTextColor }
+    val takenTrailingIconColor = trailingIconColor.takeOrElse { takenTextColor }
+
+    val textColor
+            by rememberUpdatedState(
+                if (enabled) takenTextColor
+                else takenTextColor.copy(alpha = .3f)
+            )
+    val leadingColor
+            by rememberUpdatedState(
+                if (enabled) takenLeadingIconColor
+                else takenLeadingIconColor.copy(alpha = .3f)
+            )
+    val trailingColor
+            by rememberUpdatedState(
+                if (enabled) takenTrailingIconColor
+                else takenTrailingIconColor.copy(alpha = .3f)
+            )
 
     Row(
+        modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        leftIcon?.let { imageVector ->
+        leadingIcon?.let { imageVector ->
             Icon(
+                modifier = modifier
+                    .weight(1f, false)
+                    .align(iconAlignment),
                 imageVector = imageVector,
                 contentDescription = imageVector.name,
-                tint = textColor
+                tint = leadingColor
             )
         }
-        BadgedBox(
-            modifier = if (badge) Modifier.padding(end = 4.dp) else Modifier,
-            badge = {
-                if (badge) {
-                    Badge(
-                        modifier = Modifier
-                            .size(4.dp)
-                            .offset(4.dp, 4.dp),
-                        backgroundColor = red60
+        Row(
+            modifier = modifier.weight(8f, false),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            BadgedBox(
+                modifier = if (badge) Modifier.padding(end = 4.dp) else Modifier,
+                badge = {
+                    if (badge) {
+                        Badge(
+                            modifier = Modifier
+                                .size(4.dp)
+                                .offset(4.dp, 4.dp),
+                            backgroundColor = red60
 
+                        )
+                    }
+                },
+                content = {
+                    Text(
+                        text = text,
+                        color = textColor,
+                        style = style,
+                        onTextLayout = onTextLayout,
+                        overflow = overflow,
+                        softWrap = softWrap,
+                        maxLines = maxLines,
                     )
                 }
-            },
-            content = {
-                Text(
-                    modifier = modifier,
-                    text = text.toString(),
-                    color = rememberTextColor,
-                    style = style,
-                    onTextLayout = onTextLayout,
-                    overflow = overflow,
-                    softWrap = softWrap,
-                    maxLines = maxLines,
-                )
-            }
-        )
-        count?.let {
-            countColors?.let {
-                GfCount(
-                    count = count,
-                    colors = countColors,
-                    enabled = enabled,
-                )
+            )
+            count?.let {
+                countColors?.let {
+                    GfCount(
+                        count = count,
+                        colors = countColors,
+                        enabled = enabled,
+                    )
+                }
             }
         }
-        rightIcon?.let { imageVector ->
+        trailingIcon?.let { imageVector ->
             Icon(
+                modifier = modifier
+                    .weight(1f, false)
+                    .align(iconAlignment),
                 imageVector = imageVector,
                 contentDescription = imageVector.name,
-                tint = textColor
+                tint = trailingColor
             )
         }
     }
@@ -123,35 +157,45 @@ fun GfTextPreview() {
             color = GfTheme.colorScheme.container.background
         ) {
             Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
                 verticalArrangement = Arrangement.spacedBy(4.dp)
             ) {
                 GfText(
-                    leftIcon = Icons.Filled.Menu,
-                    text = "텍스트",
+                    modifier = Modifier.fillMaxWidth(),
+                    leadingIcon = Icons.Filled.Menu,
+                    text = "텍스트텍스트텍스트텍스트텍스트텍스트텍스트텍스트텍스트텍스트텍스트텍스트텍스트텍스트",
                     badge = true,
                     count = 3,
                     style = GfTheme.typoScheme.headline.largeBold,
                     countColors = GfCountDefaults.Colors.neutral,
-                    rightIcon = Icons.Filled.ArrowDropDown
+                    trailingIcon = Icons.Filled.ArrowDropDown,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2,
+                    iconAlignment = Alignment.Top,
                 )
 
                 GfText(
-                    leftIcon = Icons.Filled.Menu,
+                    modifier = Modifier.width(200.dp),
+                    leadingIcon = Icons.Filled.Menu,
                     text = "텍스트",
                     count = 3,
+                    badge = true,
                     style = GfTheme.typoScheme.headline.largeBold,
                     countColors = GfCountDefaults.Colors.neutral,
-                    rightIcon = Icons.Filled.ArrowDropDown
+                    trailingIcon = Icons.Filled.ArrowDropDown
                 )
 
                 GfText(
-                    leftIcon = Icons.Filled.Menu,
+                    modifier = Modifier.wrapContentSize(),
+                    leadingIcon = Icons.Filled.Menu,
                     text = "텍스트",
                     badge = true,
                     count = 3,
                     style = GfTheme.typoScheme.body.smallMedium,
                     countColors = GfCountDefaults.Colors.neutral,
-                    rightIcon = Icons.Filled.ArrowDropDown
+                    trailingIcon = Icons.Filled.ArrowDropDown
                 )
             }
         }
