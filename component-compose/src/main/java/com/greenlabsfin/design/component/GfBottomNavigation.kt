eac8cdceb5d0b786e16297e589e256bc552f834c
tae <taewoo.kim@greenlabsfin.com>
2022-12-27 18:57:56 +0900

    [FEAT] Bottom Navigation 추가

---
+++ component-compose/src/main/java/com/greenlabsfin/design/component/GfBottomNavigation.kt
@@ -0,0 +1,256 @@
+package com.greenlabsfin.design.component
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.greenlabsfin.design.core.GfTheme
+import com.greenlabsfin.design.core.LocalGfBackgroundColor
+import com.greenlabsfin.design.core.color.red60
+
+object GfBottomNavigationDefaults {
+    val elevation = 12.dp
+    val height = 80.dp
+    val contentPadding = PaddingValues(top = 12.dp)
+    val arrangement = Arrangement.spacedBy(3.dp)
+}
+
+object GfBottomNavigation {
+    enum class IconSize(val size: Dp) {
+        Large(24.dp),
+        Small(20.dp),
+        ;
+
+        val countSize: GfCount.Size
+            get() = when (this) {
+                Large -> GfCount.Size.Large
+                Small -> GfCount.Size.Medium
+            }
+
+        val badgeRadius: Dp
+            get() = when (this) {
+                Large -> 3.dp
+                Small -> 2.dp
+            }
+    }
+}
+
+@Composable
+fun GfBottomNavigation(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = LocalGfBackgroundColor.current,
+    elevation: Dp = GfBottomNavigationDefaults.elevation,
+    listState: LazyListState = rememberLazyListState(),
+    hideWhileScrollUp: Boolean = false,
+    content: @Composable RowScope.() -> Unit,
+) {
+    var isUpScrolling by remember { mutableStateOf(false) }
+    var firstVisibleItemIndex by remember { mutableStateOf(0) }
+    var scrollOffset by remember { mutableStateOf(0) }
+    LaunchedEffect(listState) {
+        snapshotFlow { listState.firstVisibleItemIndex to listState.firstVisibleItemScrollOffset }
+            .collect {
+                val index = it.first
+                val offset = it.second
+                if (index != firstVisibleItemIndex) {
+                    scrollOffset = offset
+                }
+                firstVisibleItemIndex = index
+                val offsetDelta = offset.minus(scrollOffset)
+                if (offsetDelta == 0) return@collect
+                val isDown = offsetDelta < GfTopBarDefaults.scrollThreshold.unaryMinus()
+                val isUp = offsetDelta > GfTopBarDefaults.scrollThreshold
+                isUpScrolling = if (isUpScrolling) {
+                    isDown.not()
+                } else {
+                    isUp
+                }
+            }
+    }
+
+    val heightPixel = with(LocalDensity.current) { GfBottomNavigationDefaults.height.toPx() }
+    val yPosition by animateFloatAsState(
+        if (isUpScrolling && hideWhileScrollUp) heightPixel
+        else 0f
+    )
+
+    Surface(
+        color = backgroundColor,
+        elevation = elevation,
+        modifier = modifier.graphicsLayer { translationY = yPosition },
+        shape = RoundedCornerShape(topEnd = 12.dp, topStart = 12.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(GfBottomNavigationDefaults.height)
+                .selectableGroup(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            content = content,
+        )
+    }
+}
+
+@Composable
+fun RowScope.GfBottomNavigationItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    selectedIcon: ImageVector,
+    unselectedIcon: ImageVector,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    selectedContentColor: Color = GfTheme.colorScheme.contents.neutralPrimary,
+    unselectedContentColor: Color = GfTheme.colorScheme.contents.neutralSecondary,
+    enabled: Boolean = true,
+    count: Int? = null,
+    countColor: GfCountColors = GfCountDefaults.Colors.errorSecondary,
+    badge: Boolean = false,
+    label: @Composable (() -> Unit)? = null,
+    alwaysShowLabel: Boolean = true,
+    iconSize: GfBottomNavigation.IconSize = GfBottomNavigation.IconSize.Small,
+    contentPadding: PaddingValues = GfBottomNavigationDefaults.contentPadding,
+    verticalArrangement: Arrangement.Vertical = GfBottomNavigationDefaults.arrangement,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    Box(
+        modifier = modifier
+            .selectable(
+                selected = selected,
+                onClick = onClick,
+                enabled = enabled,
+                role = Role.Tab,
+                interactionSource = interactionSource,
+                indication = null,
+            )
+            .weight(1f)
+            .align(Alignment.CenterVertically),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(contentPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = verticalArrangement,
+        ) {
+            Box(
+                modifier = Modifier.size(iconSize.size)
+            ) {
+                GfBottomNavigationIcon(
+                    iconSize = iconSize,
+                    selected = selected,
+                    selectedIcon = selectedIcon,
+                    unselectedIcon = unselectedIcon,
+                    contentDescription = contentDescription,
+                    selectedContentColor = selectedContentColor,
+                    unselectedContentColor = unselectedContentColor,
+                    badge = badge,
+                    count = count,
+                    countColor = countColor,
+                )
+            }
+            if (alwaysShowLabel || selected) {
+                label?.let {
+                    Crossfade(targetState = selected) { selected ->
+                        Decoration(
+                            contentColor = if (selected) selectedContentColor else unselectedContentColor,
+                            typography = GfTheme.typoScheme.caption.xSmallRegular.merge(TextStyle(
+                                color = if (selected) selectedContentColor else unselectedContentColor))
+                        ) {
+                            Box(
+                                modifier = Modifier.height(18.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                label()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun BoxScope.GfBottomNavigationIcon(
+    iconSize: GfBottomNavigation.IconSize,
+    selected: Boolean,
+    selectedIcon: ImageVector,
+    unselectedIcon: ImageVector,
+    contentDescription: String?,
+    selectedContentColor: Color,
+    unselectedContentColor: Color,
+    badge: Boolean,
+    count: Int?,
+    countColor: GfCountColors,
+) {
+    Crossfade(targetState = selected) {
+        GfIcon(
+            modifier = Modifier
+                .size(iconSize.size)
+                .align(Alignment.Center),
+            imageVector = if (it) selectedIcon else unselectedIcon,
+            contentDescription = contentDescription,
+            tint = if (it) selectedContentColor else unselectedContentColor,
+        )
+    }
+    if (badge) {
+        Canvas(modifier = Modifier.offset(
+            x = iconSize.size,
+            y = 0.dp
+        ), onDraw = {
+            drawCircle(
+                color = red60,
+                radius = iconSize.badgeRadius.toPx()
+            )
+        })
+    }
+    count?.let {
+        GfCount(
+            modifier = Modifier
+                .offset(
+                    x = 11.dp,
+                    y = 5.dp.unaryMinus()
+                ),
+            size = iconSize.countSize,
+            count = count,
+            colors = countColor
+        )
+    }
+}
