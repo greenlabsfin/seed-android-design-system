e23fd36d5ef1a0062587ad98f934c406f9d11ba6
tae <taewoo.kim@greenlabsfin.com>
2022-12-27 18:56:46 +0900

    [FEAT] Background Decoration 유틸 추가

---
+++ component-compose/src/main/java/com/greenlabsfin/design/component/util/Decoration.kt
@@ -0,0 +1,38 @@
+package com.greenlabsfin.design.component.util
+
+import android.app.Activity
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import com.greenlabsfin.design.core.LocalGfBackgroundColor
+
+@Composable
+fun DecorateBackground(
+    color: Color,
+    content: @Composable () -> Unit,
+) {
+    CompositionLocalProvider(
+        LocalGfBackgroundColor provides color,
+        content = {
+            ChangeStatusBarColor(color)
+            Surface(
+                color = color,
+                content = content,
+            )
+        }
+    )
+}
+
+@Composable
+fun ChangeStatusBarColor(color: Color) {
+    val view = LocalView.current
+    if (view.isInEditMode.not()) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = color.toArgb()
+        }
+    }
+}
